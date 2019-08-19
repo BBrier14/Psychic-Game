bab3a2bd2a94e0bd4b2c5f7ad52d26ca3d152dd8
Benjamin Brier <ben.brier14@gmail.com>
2019-08-19 18:02:32 -0400

    textcontent issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,42 @@
 // I want to give the computer options to pick from 
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //I want to create variables that will change based the rules of the game
-var win = 0;
+var wins = 0;
 var loss = 0;
 var guessesLeft = 10;
 var guessedLetters = [];
 
 //I need the numbers to change on the html page
-var winsText = document.getElementById("wins-text");
-var lossText = document.getElementById("losses-text");
-var guessLeftText = document.getElementById("guessleft-text");
-var guessesText = document.getElementById("guessupdate-text");
+// var winsText = document.getElementById("wins-text");
+// var lossText = document.getElementById("losses-text");
+// var guessLeftText = document.getElementById("guessleft-text");
+// var guessesText = document.getElementById("guessupdate-text");
 
 // Need to create a function for every time the user presses a key that includes variables
 document.onkeyup = function(event) {
 
-var userGuess = event.key;
-guessedLetters.push(userGuess);
+    var userGuess = event.key;
+    guessedLetters.push(userGuess);
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-if (userGuess === computerGuess) {
-    win++;
-    guessesLeft = 9}
+    if (userGuess === computerGuess) {
+        wins++;
+        guessesLeft = 9
+    }
 
-else {guessesLeft--}
+    else { guessesLeft-- }
+
+    winsText.textContent = "you chose" + wins;
+
+
+var winsText = document.getElementById("wins-text");
+var lossText = document.getElementById("losses-text");
+var guessLeftText = document.getElementById("guessleft-text");
+var guessesText = document.getElementById("guessupdate-text");
+    
 }
 
 
 // Need the guesses and information to be displayed
-winsText.textContent = wins;
