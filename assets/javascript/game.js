38217e148066d02c427ab44406922dda7746aaa8
Benjamin Brier <ben.brier14@gmail.com>
2019-08-19 16:22:18 -0400

    created starting variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// I want to give the computer options to pick from 
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+//I want to create variables that will change based the rules of the game
+var win = 0
+var loss = 0
+var guessesLeft = 10
+var guessedLetters = []
