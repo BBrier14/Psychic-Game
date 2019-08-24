7f872d509451c641b75720713285aa660b6e128a
Benjamin Brier <ben.brier14@gmail.com>
2019-08-24 17:07:37 -0400

    Complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,28 +19,29 @@ document.onkeyup = function (event) {
 
 
 
-// The computer needs to randomize choices
+    // The computer needs to randomize choices
 
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    
 
 
-//  Only run my code if the follows buttons are pressed
+
+    //  Only run my code if the follows buttons are pressed
     if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") ||
-    (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || 
-    (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || 
-    (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || 
-    (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || 
-    (userGuess === "z")) {
+        (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") ||
+        (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") ||
+        (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") ||
+        (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") ||
+        (userGuess === "z")) {
 
-        for (i = 0; i < guessLetters.length; i++){
-            if (userGuess === guessLetters[i]){
-                alert ("No repeats!");
-                return(userGuess)
-            }}
-        
+        for (i = 0; i < guessLetters.length; i++) {
+            if (userGuess === guessLetters[i]) {
+                alert("No repeats!");
+                return (userGuess)
+            }
+        }
 
- // Create "if" statements that the computer will respond to
+
+        // Create "if" statements that the computer will respond to
         if (userGuess === computerGuess) {
             numberGuesses = 10;
             guessLetters = [];
@@ -81,8 +82,8 @@ document.onkeyup = function (event) {
         //every time a key is pressed i want to loop through this array and check if the key that i pressed is in the array
         // if it is in the array, then i dont want to run my logic, otherwise, run my logic
 
-        
-            
+
+
 
 
         // Need the guesses and information to be displayed
