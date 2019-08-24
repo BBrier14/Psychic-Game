66fc03c54c95287325186b633510ec434c9e7ec9
Benjamin Brier <ben.brier14@gmail.com>
2019-08-24 16:52:24 -0400

    fixed alphabet issue and repetition issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,10 @@ var loss = 0;
 var numberGuesses = 10;
 var guessLetters = [];
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess)
 
+//II need the computer to randomize its choices
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess)
 
 // Need to create a function for every time the user presses a key that includes variables
 document.onkeyup = function (event) {
@@ -18,70 +19,84 @@ document.onkeyup = function (event) {
 
 
 
-    // The computer needs to randomize choices
-    
+// The computer needs to randomize choices
+
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-    // Create "if" statements that the computer will respond to
-
-    // if (userGuess !== letters) {
-    //     alert("stop");
-    // }
-
     
-    if (userGuess === computerGuess) {
-        numberGuesses = 10;
-        guessLetters = [];
-        wins++;
-        alert("You did it! You guessed " + "'" + userGuess + "'" + "! Was it luck or do you have the gift? Try again!");
-        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerGuess);
-    }
 
-    if (userGuess != computerGuess) {
-        numberGuesses--;
-        guessLetters.push(userGuess);
 
-     if (numberGuesses < 1) {
-         numberGuesses = 10;
-         guessLetters = []
-        loss++
-        alert("Sorry, maybe this psychic thing isn't for you.")
-        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerGuess);
+//  Only run my code if the follows buttons are pressed
+    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") ||
+    (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || 
+    (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || 
+    (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || 
+    (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || 
+    (userGuess === "z")) {
+
+        for (i = 0; i < guessLetters.length; i++){
+            if (userGuess === guessLetters[i]){
+                alert ("No repeats!");
+                return(userGuess)
+            }}
+        
+
+ // Create "if" statements that the computer will respond to
+        if (userGuess === computerGuess) {
+            numberGuesses = 10;
+            guessLetters = [];
+            wins++;
+            alert("You did it! You guessed " + "'" + userGuess + "'" + "! Was it luck or do you have the gift? Try again!");
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            console.log(computerGuess);
         }
-    }
 
-    if (wins > 9) {
-        alert("You truly are gifted! Go play the lottery!");
-        wins = 0;
-        loss = 0;
+        if (userGuess != computerGuess) {
+            numberGuesses--;
+            guessLetters.push(userGuess);
+
+            if (numberGuesses < 1) {
+                numberGuesses = 10;
+                guessLetters = []
+                loss++
+                alert("Sorry, maybe this psychic thing isn't for you.")
+                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                console.log(computerGuess);
+            }
+        }
+
+        if (wins > 9) {
+            alert("You truly are gifted! Go play the lottery!");
+            wins = 0;
+            loss = 0;
+
+        }
+        if (loss > 9) {
+            alert("Maybe it's time to accept that you should stick to your day job")
+            wins = 0;
+            loss = 0;
+
+        }
+
+        //every time a key is pressed I want to store it in an array,
+        //every time a key is pressed i want to loop through this array and check if the key that i pressed is in the array
+        // if it is in the array, then i dont want to run my logic, otherwise, run my logic
+
         
+            
+
+
+        // Need the guesses and information to be displayed
+        document.getElementById("wins-text").textContent = "Wins: " + wins;
+        document.getElementById("losses-text").textContent = "Losses: " + loss;
+        document.getElementById("guessleft-text").textContent = "Guesses Left: " + numberGuesses;
+        document.getElementById("guessupdate-text").textContent = "Guesses: " + guessLetters;
+
+
+
+
     }
-    if (loss > 9) {
-        alert("Maybe it's time to accept that you should stick to your day job")
-        wins = 0;
-        loss = 0;
-        
-    }
-
-//every time a key is pressed I want to store it in an array,
-//every time a key is pressed i want to loop through this array and check if the key that i pressed is in the array
-// if ti is in the array, then i dont want to run my logic, otherwise, run my logic
-
-
-    // Need the guesses and information to be displayed
-    document.getElementById("wins-text").innerHTML = "Wins: " + wins;
-    document.getElementById("losses-text").innerHTML = "Losses: " + loss;
-    document.getElementById("guessleft-text").innerHTML = "Guesses Left: " + numberGuesses;
-    document.getElementById("guessupdate-text").innerHTML = "Guesses: " + guessLetters;
-
-
-
-
-
-
 }
+
 
 
 
